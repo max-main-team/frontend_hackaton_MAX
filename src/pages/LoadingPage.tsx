6d1a6883373d28df922131fc052bd308ea6e60ca
nikitos212 <nikita.08t07@gmail.com>
2025-11-10 20:11:12 +0300

    fix css

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,15 +1,15 @@
-import { Panel, Container, Flex, Typography } from "@maxhub/max-ui";
+import { Panel, Container, Typography } from "@maxhub/max-ui";
 import '@maxhub/max-ui/dist/styles.css';
 import '../css/MainLayout.css';
 
 export default function LoadingPage() {
   return (
-    <Panel>
-      <Container>
-        <Flex>
+    <div className="fullscreen-overlay">
+      <Panel className="panel-inner" mode="secondary">
+        <Container style={{ textAlign: "center" }}>
           <Typography.Title variant="large-strong">Загрузка...</Typography.Title>
-        </Flex>
-      </Container>
-    </Panel>
+        </Container>
+      </Panel>
+    </div>
   );
 }
