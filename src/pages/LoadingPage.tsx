0cdde53dddf6519bf1d4343a8e2e4cd355e717a3
nikitos212 <nikita.08t07@gmail.com>
2025-11-10 20:05:29 +0300

    fix css

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,14 +1,15 @@
 import { Panel, Container, Flex, Typography } from "@maxhub/max-ui";
+import '@maxhub/max-ui/dist/styles.css';
 import '../css/MainLayout.css';
 
 export default function LoadingPage() {
   return (
-        <Panel>
-          <Container>
-            <Flex>
-              <Typography.Title variant="large-strong">Загрузка...</Typography.Title>
-            </Flex>
-          </Container>
-        </Panel>
+    <Panel>
+      <Container>
+        <Flex>
+          <Typography.Title variant="large-strong">Загрузка...</Typography.Title>
+        </Flex>
+      </Container>
+    </Panel>
   );
 }
