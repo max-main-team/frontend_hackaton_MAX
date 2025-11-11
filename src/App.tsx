c81bfa369b5d7c150aa7700c8800847c746e59f9
nikitos212 <nikita.08t07@gmail.com>
2025-11-11 20:12:27 +0300

    fix useMaxWebApp

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function AppInner() {
   useEffect(() => {
     setLoading(true);
 
-    api.post("/api/auth/login", window.WebApp?.initData)
+    api.post("https://msokovykh.ru/auth/login", window.WebApp?.initData)
       .then(async res => {
         const access = res.data?.access_token;
         const roles: string[] = res.data?.user_roles;
