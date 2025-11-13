c3354854d01415c9937c6fec3b6e56436e352da8
nikitos212 <nikita.08t07@gmail.com>
2025-11-13 20:50:22 +0300

    feat

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,12 +1,15 @@
 import '../css/MainLayout.css';
-import { Panel, Container, Typography } from "@maxhub/max-ui";
+import { Panel, Container, Spinner } from "@maxhub/max-ui";
 
 export default function LoadingPage() {
   return (
     <div className="fullscreen-overlay" >
       <Panel className="panel-inner" mode="secondary" centeredX centeredY>
-        <Container style={{ textAlign: "center", padding: 28 }}>
-          <Typography.Title variant="large-strong">Загрузка...</Typography.Title>
+        <Container style={{ textAlign: "center", padding: 20 }}>
+          <Spinner
+            appearance="neutral-themed"
+            size={40}
+          />
         </Container>
       </Panel>
     </div>
--- src/pages/StudentPage.tsx
+++ src/pages/StudentPage.tsx
@@ -1,10 +1,29 @@
-
-import { Button } from "@maxhub/max-ui";
 import "../css/AdminPage.css";
+import { Flex, Avatar, Typography, Button } from "@maxhub/max-ui";
+import { useMaxWebApp } from "../hooks/useMaxWebApp";
+
 
 export default function StudentPage() {
+  const { webAppData } = useMaxWebApp();
+  const user = webAppData?.user ?? {};
+  const name = user?.first_name ? `${user.first_name}${user.last_name ? " " + user.last_name : ""}` : "Студент";
 
   return (
-    <Button>R</Button>
+      <div style={{ marginBottom: 12 }}>
+        <Flex align="center" gap={12}> 
+          <Avatar.Container size={56} form="circle">
+            <Avatar.Image src={user?.full_avatar_url ?? user?.avatar_url ?? ""} />
+          </Avatar.Container>
+
+          <div>
+            <Typography.Title variant="large-strong">Привет, {name}!</Typography.Title>
+            <Typography.Label>{user?.university ?? ""}</Typography.Label>
+          </div>
+
+          <div style={{ marginLeft: "auto" }}>
+            <Button>Мои курсы</Button>
+          </div>
+        </Flex>
+      </div>
   );
 }
